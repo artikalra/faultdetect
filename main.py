b81e1b1a7b6b9771c178ba8032e2467fff8750d4
artikalra <arti.kalra@student.isae-supaero.fr>
2018-07-17 18:10:00 +0530

    First Version: Computation of L2 distances

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import util.filereader as rd
+
 if __name__ == '__main__':
-    print("hello")
+    samples = rd.parse("data/17_07_06__10_21_07_SD.data")
+   # print(samples._data[:1][0][0])
+   # print(samples[33.38:330.513])
+   # print(samples[33.447])
+    print(range(len(list(samples))))
+    print(samples.distance())
+
+
