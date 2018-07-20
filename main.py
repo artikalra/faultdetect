4341e46e1f3bafd33fc0bb46a1630853ada194f5
artikalra <arti.kalra@student.isae-supaero.fr>
2018-07-20 18:02:42 +0530

    Calculation of the the distances for all the trajectories( symmetry and zero distance verified)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import util.filereader as rd
-from scipy.integrate import quad
+#from scipy.integrate import quad
 import math
 import numpy as np
+from data.cluster import Clustering
 
 if __name__ == '__main__':
     samples = rd.parse("data/17_07_06__10_21_07_SD.data")
@@ -21,21 +22,40 @@ if __name__ == '__main__':
         #print(finaldistances)
 
 #Integration of the square of L2norm for all the distances
+    #from sympy import *
+    #finaldistances=[]
+    #for time in range(5):
+        #for m in range(1, 6):
+            #for n in range(1,6):
+                #for k in range(6):
+                #print(samples._data[:m][m-1][0])
+                #print(samples._data[:(n)][n-1][0])
+                #dist = sum([(a - b) ** 2 for a, b in zip(samples._data[:m][m - 1], samples._data[:(n + 1)][n])])
+                #dist = sum([(a - b) ** 2 for a, b in zip(samples._data[:m][m - 1], samples._data[:n][n-1])])
+            #t = Symbol('t')
+            #ans = integrate(dist, (t , samples._times[time], samples._times[time+5]))
+            #sol = math.sqrt(ans/(samples._times[time+5]-samples._times[time]))
+            #finaldistances.append(sol)
+    #print(np.reshape(np.asarray(finaldistances),(5,5)))
+
+
+# to compute to integration of the square of L2norm for all the trajectories
     from sympy import *
-    finaldistances=[]
-    for time in range(50):
-        for m in range(1, 51):
-            for n in range(1,51):
-                # for k in range(6):
-                dist = sum([(a - b) ** 2 for a, b in zip(samples._data[:m][m - 1], samples._data[:(n + 1)][n])])
-                t = Symbol('t')
-                ans = integrate(dist, (t , samples._times[time], samples._times[time+10]))
-                sol = math.sqrt(ans/(samples._times[time+10]-samples._times[time]))
+    finaldistances = []
+    for m in range(1, 101):
+        for n in range(1, 101):
+            # dist = sum([(a - b) ** 2 for a, b in zip(samples._data[:m][m - 1], samples._data[:(n + 1)][n])])
+            dist = sum([(a - b) ** 2 for a, b in zip(samples._data[:m][m - 1], samples._data[:n][n - 1])])
+            t = Symbol('t')
+            ans = integrate(dist, (t, samples._data[:m][m-1][0], samples._data[:(m+5)][m][0]))
+            sol = math.sqrt(ans / (samples._data[:(m+5)][m][0] - samples._data[:m][m-1][0]))
             finaldistances.append(sol)
-    print(np.reshape(np.asarray(finaldistances),(50,50)))
+    distanceMatrix = np.reshape(np.asarray(finaldistances), (100, 100))
+    print(distanceMatrix)
 
 
 
+#print(Clustering(4,distanceMatrix))
 
 
 
