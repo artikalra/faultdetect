Distance between the quaternions logs using inbuilt function (giving incorrect distance matrix)

main.py
@@ -43,6 +43,7 @@ if __name__ == '__main__':
     quatern_log = []
     #quatern_dist = np.zeros((5,))
     quatern_dist = []
+    quat_dist =[]
 
     quatern_fault[:,0] = [1.,0.,0.,0.]
     # h is the time step, delta_t, which is constant for the momoent as 0.02s (50Hz of data acquisition), but you can also make it variable too
@@ -60,22 +61,25 @@ if __name__ == '__main__':
             quatern_log.append(log)
 
             #quatern_log[:, i] = Quaternion.log(Quaternion((quatern_fault[:,i])))
-            #quatern_dist = Quaternion.distance(Quaternion((quatern_fault[:,i])),Quaternion((quatern_fault[:,i-1])))
-            #print(quatern_dist)
+            quatern_dist = Quaternion.distance(Quaternion((quatern_fault[:,i])),Quaternion((quatern_fault[:,n])))
+            quat_dist.append(quatern_dist)
+    quatdistanceMatrix = np.reshape(np.asarray(quat_dist), (4, 4))
+    print(quatdistanceMatrix)
+    #print(quat_dist)
 
-            list_log = np.ndarray.tolist(log.vector)
+            #list_log = np.ndarray.tolist(log.vector)
 
-            dist = sum([(a - b) ** 2 for a, b in zip(list_log[:i][0:3], list_log[:n][0:3])])
-            t = Symbol('t')
-            ans = integrate(dist, (t,samples._data[:i][i - 1][0], samples._data[:(i + 5)][i][0]))
-            sol = math.sqrt(ans / (samples._data[:(i + 5)][i][0] - samples._data[:i][i- 1][0]))
-            quatern_dist.append(sol)
+            #dist = sum([(a - b) ** 2 for a, b in zip(list_log[:i][0:3], list_log[:n][0:3])])
+            #t = Symbol('t')
+            #ans = integrate(dist, (t,samples._data[:i][i - 1][0], samples._data[:(i + 5)][i][0]))
+            #sol = math.sqrt(ans / (samples._data[:(i + 5)][i][0] - samples._data[:i][i- 1][0]))
+            #quatern_dist.append(sol)
 
-    quatdistanceMatrix = np.reshape(np.asarray(quatern_dist), (4, 4))
+    #quatdistanceMatrix = np.reshape(np.asarray(quatern_dist), (4, 4))
 
     #print((quatern_fault))
     #print(quatern_log)
-    print(quatdistanceMatrix)
+    #print(quatdistanceMatrix)
 
 
 