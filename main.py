After correcting the spinnors calculation

main.py
@@ -20,18 +20,27 @@ if __name__ == '__main__':
     quatern_fault[:, 0] = [1., 0., 0., 0.]
     h = 0.2
 
-    for i in range(1, (len(samples._data))):
+    for i in range(1, 10):
         if i % 100 == 0:
             print('spin ', i)
         quatern_fault[:, i] = samples.RK4(samples.KinematicModel, np.array(samples._data[:i][i - 1][1:4]),
                                           quatern_fault[:, i - 1], h)
+        print(quatern_fault.shape)
         my_quaternion = Quaternion(quatern_fault[:, i])
-        spinnors[:,i]= Quaternion.log(my_quaternion)
-        quatspinnors[:,i] = spinnors[1:4,i]
+        #print(my_quaternion)
+        quatspinnors[:, i] = Quaternion.log(my_quaternion).elements[1:4]
+        print(quatspinnors)
+
+        #quatspinnors[:, i] = spinnors[1:4,i]
+        #print(quatspinnors.shape)
     samples._data = np.hstack((np.array(samples._data),quatspinnors.T))
     print((samples._data).shape)
     print(samples._data)
 
+    qlog1 = Quaternion.log(Quaternion([0.988,0.085,0.100,0.083]))
+    qlog2 = Quaternion.log(Quaternion([1.0, 0.0 , 0.0, 0.0]))
+    print(qlog1, qlog2)
+    print(Quaternion([0.988,0.085,0.100,0.083]))
 
     ## Downsampling
     breakn = 10